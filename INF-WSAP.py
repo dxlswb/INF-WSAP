ffad7af28e3dc4f9dce160ad2c717a47d4401be8
dxlswb <103540884+dxlswb@users.noreply.github.com>
2022-04-12 22:23:16 -0400

    Update and rename README.md to INF-WSAP.py

---
+++ INF-WSAP.py
@@ -0,0 +1,22 @@
+import requests
+import os
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
+RESET = '\033[39m'
+os.system("clear")
+
+print(BLUE+"█ █▄░█ █▀▀ ▄▄ █░█░█ █▀ █▀█")
+pronto("    █ █░▀█ █▀░ ░░ ▀▄▀▄▀ ▄█ █▀▀")
+
+print("\033[33m--------------------------------------")
+print(RED+"Autor:dxlswb")
+print("--------------------------------------")
+print(WHITE+"TIK TOK: dxlswb")
+print("--------------------------------------")
+print(WHITE+"escribe el numero de el whatsapp junto\ncon el prefijo, ejemplo: +526621745707\n")
